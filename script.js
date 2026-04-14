9d8d78ac89b3607e97c7cf47fe20ef3cae138842
reehalrohit <76120137+reehalrohit@users.noreply.github.com>
2026-04-14 13:42:13 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,12 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    display.value = eval(display.value);
+}
